db24feecaf007914d576b6ad479fd80c345c1824
Brandon Ros <brandonros1@gmail.com>
2025-08-20 21:35:31 -0400

    different GetStartupInfoA

--- src/winapi/kernel32/GetStartupInfoA.rs
+++ src/winapi/kernel32/GetStartupInfoA.rs
@@ -1,6 +1,8 @@
 use unicorn_engine::Unicorn;
 use unicorn_engine::RegisterX86;
-use crate::winapi::structures::StartupInfo64;
+use windows_sys::Win32::System::Threading::STARTUPINFOA;
+
+use crate::winapi::structures;
 
 pub fn GetStartupInfoA(emu: &mut Unicorn<()>) -> Result<(), unicorn_engine::uc_error> {
     // Get the pointer to STARTUPINFO structure from RCX register
@@ -9,11 +11,27 @@ pub fn GetStartupInfoA(emu: &mut Unicorn<()>) -> Result<(), unicorn_engine::uc_e
     log::info!("[GetStartupInfoA] startup_info_ptr: 0x{:x}", startup_info_ptr);
     
     if startup_info_ptr > 0 {
-        // Create a new StartupInfo64 structure with default values
-        let startup_info = StartupInfo64::new();
-        
-        // Save the structure to the emulator memory at the provided address
-        startup_info.save(emu, startup_info_ptr);
+        let mock_startup_info = STARTUPINFOA {
+            cb: std::mem::size_of::<STARTUPINFOA>() as u32,  // Correct size
+            lpReserved: std::ptr::null_mut(),
+            lpDesktop: std::ptr::null_mut(),
+            lpTitle: std::ptr::null_mut(),
+            dwX: 10,
+            dwY: 10,
+            dwXSize: 300,
+            dwYSize: 200,
+            dwXCountChars: 0,
+            dwYCountChars: 0,
+            dwFillAttribute: 0,
+            dwFlags: 0,  // or use specific STARTUPINFOW_FLAGS if needed
+            wShowWindow: 1,
+            cbReserved2: 0,
+            lpReserved2: std::ptr::null_mut(),
+            hStdInput: std::ptr::null_mut(),
+            hStdOutput: std::ptr::null_mut(),
+            hStdError: std::ptr::null_mut(),
+        };
+        structures::write_struct(emu, startup_info_ptr, &mock_startup_info);
     }
     
     Ok(())
--- src/winapi/structures/mod.rs
+++ src/winapi/structures/mod.rs
@@ -1,5 +1,14 @@
 #![allow(non_snake_case)]
 
-#[path = "StartupInfo64.rs"]
-mod startup_info_64;
-pub use startup_info_64::StartupInfo64;
+use unicorn_engine::Unicorn;
+
+pub fn write_struct<T>(emu: &mut Unicorn<()>, addr: u64, data: &T) {
+    let size = std::mem::size_of::<T>();
+    let bytes: &[u8] = unsafe {
+        std::slice::from_raw_parts(
+            data as *const T as *const u8,
+            size
+        )
+    };
+    emu.mem_write(addr, bytes).unwrap();
+}
